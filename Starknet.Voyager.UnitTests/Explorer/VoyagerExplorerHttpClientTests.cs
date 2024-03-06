e2158a8799c75609404a96c86c43aa7efff3dfda
DimSoftware <dimsoft@protonmail.com>
2024-03-06 15:39:05 +0200

    Added Unit Tests project

---
+++ Starknet.Voyager.UnitTests/Explorer/VoyagerExplorerHttpClientTests.cs
@@ -0,0 +1,29 @@
+﻿using Starknet.Voyager.Explorer;
+using Starknet.Voyager.UnitTests.Setup;
+using WireMock.Server;
+
+namespace Starknet.Voyager.UnitTests.Explorer
+{
+    [Collection(nameof(UnitTestsCollection))]
+    public class VoyagerExplorerHttpClientTests
+    {
+        private readonly WireMockServer wireMockServer;
+        private readonly IVoyagerExplorerHttpClient voyagerExplorerHttpClient;
+
+        public VoyagerExplorerHttpClientTests(TestsSetupFixture testsSetupFixture)
+        {
+            wireMockServer = testsSetupFixture.WireMockServer;
+            voyagerExplorerHttpClient = testsSetupFixture.VoyagerExplorerHttpClient;
+        }
+
+        [Fact]
+        public void GetBlockDetailsAsyncShouldReturnSuccess()
+        {
+        }
+
+        private void Reset()
+        {
+            wireMockServer.Reset();
+        }
+    }
+}
---
+++ Starknet.Voyager.UnitTests/Setup/TestsSetupFixture.cs
@@ -0,0 +1,27 @@
+﻿using Starknet.Voyager.Explorer;
+using WireMock.Server;
+
+namespace Starknet.Voyager.UnitTests.Setup
+{
+    public class TestsSetupFixture : IDisposable
+    {
+        public WireMockServer WireMockServer { get; private set; }
+
+        public IVoyagerExplorerHttpClient VoyagerExplorerHttpClient { get; private set; }
+
+        public TestsSetupFixture()
+        {
+            WireMockServer = WireMockServer.Start();
+            VoyagerExplorerHttpClient = new VoyagerExplorerHttpClient(new HttpClient
+            {
+                BaseAddress = new Uri(WireMockServer.Urls[0])
+            });
+        }
+
+        public void Dispose()
+        {
+            WireMockServer.Stop();
+            WireMockServer.Dispose();
+        }
+    }
+}

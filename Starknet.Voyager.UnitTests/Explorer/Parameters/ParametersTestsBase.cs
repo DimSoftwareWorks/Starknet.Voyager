6a82e50c6441fe60a52e8a7942e91fb3483f18eb
DimSoftware <dimsoft@protonmail.com>
2024-03-09 16:18:12 +0200

    Added tests

---
+++ Starknet.Voyager.UnitTests/Explorer/Parameters/ParametersTestsBase.cs
@@ -0,0 +1,25 @@
+﻿using Starknet.Voyager.Attributes;
+using System.Reflection;
+
+namespace Starknet.Voyager.UnitTests.Explorer.Parameters
+{
+    public abstract class ParametersTestsBase
+    {
+        protected void TestQueryStringNameAttribute<T>(string propertyName, string expectedName)
+            where T : class
+        {
+            // Arrange
+
+            var parameters = Activator.CreateInstance<T>();
+
+            // Act
+            
+            var result = parameters?.GetType().GetProperty(propertyName)?.GetCustomAttribute<QueryStringNameAttribute>()?.Name;
+
+            // Assert
+            
+            Assert.NotNull(result);
+            Assert.Equal(expectedName, result);
+        }
+    }
+}
